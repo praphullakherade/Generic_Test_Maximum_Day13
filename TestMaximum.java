76d3fffc83ba44fcab96640dcd654020cb225af9
Praphulla Kherade <prafullkherade9@gmail.com>
2021-10-14 14:47:12 +0530

    welcome to Generics Test Maximum Program

---
+++ TestMaximum.java
@@ -0,0 +1,7 @@
+package com.generictest;
+
+public class TestMaximum {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Generics Test Maximum program");
+    }
+}
