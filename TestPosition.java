5b4c9f2ab059998e355364fb4b01e0fc741a46d6
Praphulla Kherade <prafullkherade9@gmail.com>
2021-10-17 20:15:08 +0530

    Test data max number in the second position

--- TestPosition.java
+++ TestPosition.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class TestPosition {
     @Test
     public void integerMax_AtFirstPosition() {
-        int actualResult = TestMaximum.findMaximum(56,15,30);
-        Assert.assertEquals(56,actualResult);
+        int actualResult = TestMaximum.findMaximum(56,115,30);
+        Assert.assertEquals(115,actualResult);
     }
 }
